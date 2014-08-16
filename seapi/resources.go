bc1c0f29a0252c6814857753967162c02913b278
Cyrill Schumacher <cyrill@schumacher.fm>
2014-08-16 15:15:04 +1000

    Remove unused struct properties

--- seapi/resources.go
+++ seapi/resources.go
@@ -20,49 +20,36 @@
 package seapi
 
 type Owner struct {
-	User_id       int
-	Display_name  string
 	Reputation    int
+	User_id       int
 	User_type     string //one of unregistered, registered, moderator, or does_not_exist
+	Accept_rate   int
 	Profile_image string
+	Display_name  string
 	Link          string
 }
 
-// not all fields are used ...
 type SearchResult struct {
-	Question_id          int
-	SearchResult_id      int
-	Last_edit_date       int64
-	Creation_date        int64
-	Last_activity_date   int64
-	Locked_date          int64
-	Community_owned_date int64
-	Score                int
-	Answer_count         int
-	Accepted_answer_id   int
-	Bounty_closes_date   int64
-	Bounty_amount        int
-	Closed_date          int64
-	Protected_date       int64
-	Title                string
 	Tags                 []string
-	Closed_reason        string
-	Up_vote_count        int
-	Down_vote_count      int
-	Favorite_count       int
-	View_count           int
 	Owner                Owner
-	Link                 string
 	Is_answered          bool
+	View_count           int
+	Answer_count         int
+	Score                int
+	Last_activity_date   int64
+	Creation_date        int64
+	Question_id          int
+	Link                 string
+	Title                string
 }
 
 type SearchResultCollection struct {
 	Items         []SearchResult
-	Error_id      int
-	Error_name    string
-	Error_message string
-
 	Has_more        bool
 	Quota_max       int
 	Quota_remaining int
+
+	Error_id      int
+	Error_name    string
+	Error_message string
 }
